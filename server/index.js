95ce3b7638c9489e44e168fd8f450dc323fca898
Adolf <owner@Users-MacBook-Air.local>
2021-11-12 11:27:11 -0800

    add createUser mutation

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const graphql, {GraphQLObjectType, GraphQLSchema, GraphQLInt, GraphQLString} = require('graphql');
+const graphql, {GraphQLObjectType, GraphQLSchema, GraphQLInt, GraphQLString, GraphQLList} = require('graphql');
 const { graphqlHTTP } = require('express-graphql');
 const PORT = 6969;
 const userData = require('./MOCK_DATA.json');
@@ -20,10 +20,33 @@ const UserType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
     name: "RootQueryType",
     fields: {
-        
+        getAllUsers: {
+            type: new GraphQLList(UserType),
+            args: {id: {type: GraphQLInt}},
+            resolve(parent,args) {
+                return userData
+            }
+        }
     }
 })
-const Mutation = 'mutation';
+const Mutation = new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+        createUser:{
+            type: UserType,
+            args: {
+                firstName: {type:GraphQLString},
+                lastName: {type:GraphQLString},
+                email: {type:GraphQLString},
+                password: {type:GraphQLString},
+            },
+            resolve(parent,args) {
+                userData.push({id: userData.length+1, firstName: args.firstName, lastName:args.lastName, email:args.email,password:args.password})
+                return args
+            }
+        }
+    }
+})
 
 const schema = new GraphQLSchema({ query: RootQuery, mutation: Mutation });
 
