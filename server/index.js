1d719ed4bb2baeea02696095a466471af9a61560
Adolf <owner@Users-MacBook-Air.local>
2021-11-12 11:12:08 -0800

    create UserType

--- server/index.js
+++ server/index.js
@@ -1,7 +1,39 @@
 const express = require('express');
 const app = express();
+const graphql, {GraphQLObjectType, GraphQLSchema, GraphQLInt, GraphQLString} = require('graphql');
+const { graphqlHTTP } = require('express-graphql');
 const PORT = 6969;
 const userData = require('./MOCK_DATA.json');
+
+const UserType = new GraphQLObjectType({
+    name: "User",
+    fields: () => ({
+        id: {type: GraphQLInt},
+        firstName: {type: GraphQLString},
+        lastName: {type: GraphQLString},
+        email: {type: GraphQLString},
+        password: {type: GraphQLString},
+
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        
+    }
+})
+const Mutation = 'mutation';
+
+const schema = new GraphQLSchema({ query: RootQuery, mutation: Mutation });
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 
 app.listen(PORT, () => {
   console.log('Server running...');
